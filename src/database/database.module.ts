40596d6866473d2f7d4729082f46f1fa807882eb
Jefferson Quezada <jeffersonirigoyen@gmail.com>
2025-12-20 13:02:11 -0600

    add url database

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,31 +1,19 @@
 import { Global, Module } from '@nestjs/common';
-import { ConfigType } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import config from '../config';
 
 @Global()
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      inject: [config.KEY],
-      useFactory: (configService: ConfigType<typeof config>) => {
-        const { url, host, port, database, username, password } =
-          configService.postgres;
-        return {
-          type: 'postgres',
-          url: url,
-          host: url ? undefined : host,
-          port: url ? undefined : port,
-          username: url ? undefined : username,
-          password: url ? undefined : password,
-          database: url ? undefined : database,
-
-          autoLoadEntities: true,
-          synchronize: true, // Cambia a true para que Neon cree las tablas automáticamente
-
-          // OBLIGATORIO PARA NEON
-          ssl: url ? { rejectUnauthorized: false } : false,
-        };
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: 'postgresql://neondb_owner:npg_ElwIpg2em0Vo@ep-nameless-brook-ad76ck0a-pooler.c-2.us-east-1.aws.neon.tech/mystore?sslmode=require&channel_binding=require',
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
     }),
   ],
